82b130e49de7609339e8cac211811cb825f96a63
megamp15 <47288803+megamp15@users.noreply.github.com>
2019-11-09 21:30:14 -0600

    Created an os env variable to store the bot token and github will not track the .env file

--- discordbot.py
+++ discordbot.py
@@ -1,17 +1,24 @@
+import os
 import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        print('Logged on as', self.user)
+        print("Logged on as", self.user)
 
     async def on_message(self, message):
         # don't respond to ourselves
         if message.author == self.user:
             return
-        if message.content == 'HOWDY':
-            await message.channel.send('H A W Y E E')
+        if message.content == "HOWDY":
+            await message.channel.send("H A W Y E E")
 
 
 client = MyClient()
-client.run('NjMwNTg1MTI3MDA4ODYyMjA4.XbSgiA.Cg7Z3Oa7BohiyKqd0U0abjV9ac8')
+
+
+client.run(token)
