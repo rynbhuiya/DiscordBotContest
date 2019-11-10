f0a146d9658b27de1c4ddab8968e58cae9cc027a
megamp15 <47288803+megamp15@users.noreply.github.com>
2019-11-09 19:09:19 -0600

    Changed gitignore to ignore idea and vscode setting

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,18 @@
-# Hello Hello 
+import discord
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'Hello':
+            await message.channel.send('Hello Hello')
+
+
+client = MyClient()
+client.run('NjMwNTg1MTI3MDA4ODYyMjA4.XbSgiA.Cg7Z3Oa7BohiyKqd0U0abjV9ac8')
