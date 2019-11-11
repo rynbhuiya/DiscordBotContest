e4ed5196b015fdf53ad65bb1f979a42c90097abe
megamp15 <47288803+megamp15@users.noreply.github.com>
2019-11-11 03:34:58 -0600

    Made the Tic-Tac-Toe embed edit itself and update. Issue on second player editing the board.

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 import discord.embeds
+import discord.colour
 
 # Hidden Token from the environment variables file
 load_dotenv()
@@ -31,18 +32,6 @@ on = [False]
 
 @client.event
 async def on_message(message):
-    i = 0
-    run.append(i)
-    if message.author == client.user:
-        await message.add_reaction(client.emojis[2])
-        await message.add_reaction(client.emojis[3])
-        await message.add_reaction(client.emojis[4])
-        await message.add_reaction(client.emojis[5])
-        await message.add_reaction(client.emojis[6])
-        await message.add_reaction(client.emojis[7])
-        await message.add_reaction(client.emojis[8])
-        await message.add_reaction(client.emojis[9])
-        await message.add_reaction(client.emojis[10])
     if message.author == client.user:
         return
     if message.content.startswith("/TTT"):
@@ -51,37 +40,303 @@ async def on_message(message):
         if len(userID) == 2:
             if on[0] == False:
                 on[0] = True
-                tic = discord.Embed()
+                tic = discord.Embed(colour=discord.Colour.red())
+
+                # val =""
+                # for i in range (3):
+                #     for k in range(3):
+                #         val+= arr[i][k] + " | "
+                # val+="\n"
+
+                on_message.emarr = [
+                    [   ":white_large_square:",
+                        ":white_large_square:",
+                        ":white_large_square:",
+                    ],
+                    [
+                        ":white_large_square:",
+                        ":white_large_square:",
+                        ":white_large_square:",
+                    ],
+                    [
+                        ":white_large_square:",
+                        ":white_large_square:",
+                        ":white_large_square:",
+                    ],
+                ]
                 tic.add_field(
                     name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
-                    value=":white_large_square: | :white_large_square: | :white_large_square: \n \n:white_large_square: | :white_large_square: | :white_large_square:  \n \n :white_large_square: | :white_large_square: | :white_large_square:",
+                    value=on_message.emarr[0][0]
+                    + " | "
+                    + on_message.emarr[0][1]
+                    + " | "
+                    + on_message.emarr[0][2]
+                    + "\n \n"
+                    + on_message.emarr[1][0]
+                    + " | "
+                    + on_message.emarr[1][1]
+                    + " | "
+                    + on_message.emarr[1][2]
+                    + "\n \n"
+                    + on_message.emarr[2][0]
+                    + " | "
+                    + on_message.emarr[2][1]
+                    + " | "
+                    + on_message.emarr[2][2],
                 )
-                await message.channel.send(embed=tic)
+                msg = await message.channel.send(embed=tic)
+                on_message.em = msg
+                for i in range(2, 11):
+                    await msg.add_reaction(client.emojis[i])
 
-
-# i = []
-# @client.event
-# async def on_reaction_add(reaction, user):
-#     i.append("1")
-#     if len(i) == 2:
-#         if client.emojis[2] == reaction.emoji:
-#             print("1")
-#         if client.emojis[3] == reaction.emoji:
-#             print("2")
-#         if client.emojis[4] == reaction.emoji:
-#             print("3")
-#         if client.emojis[5] == reaction.emoji:
-#             print("4")
-#         if client.emojis[6] == reaction.emoji:
-#             print("5")
-#         if client.emojis[7] == reaction.emoji:
-#             print("6")
-#         if client.emojis[8] == reaction.emoji:
-#             print("7")
-#         if client.emojis[9] == reaction.emoji:
-#             print("8")
-#         if client.emojis[10] == reaction.emoji:
-#             print("9")
+@client.event
+async def on_reaction_add(reaction, user):
+    if user == client.user:
+        return
+    print(on_reaction_add.j)
+    on_reaction_add.j += 1
+    for i in range(2, 11):
+        if reaction.emoji == client.emojis[i]:
+            if on_reaction_add.j % 2 == 0:
+                if user.name==userID[0]:
+                    v = ":x:"
+                else:
+                    on_reaction_add.j-=1
+                    return
+            else:
+                if user.name==userID[1]:
+                    v = ":o:"
+                else:
+                    on_reaction_add.j-=1
+                    return
+            print(user.name)
+            
+            if i == 2:
+                if on_message.emarr[0][0] ==":white_large_square:":
+                    on_message.emarr[0][0] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 3:
+                if on_message.emarr[0][1] ==":white_large_square:":
+                    on_message.emarr[0][1] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 4:
+                if on_message.emarr[0][2] ==":white_large_square:":
+                    on_message.emarr[0][2] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 5:
+                if on_message.emarr[1][0] ==":white_large_square:":
+                    on_message.emarr[1][0] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 6:
+                if on_message.emarr[1][1] ==":white_large_square:":
+                    on_message.emarr[1][1] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 7:
+                if on_message.emarr[1][2] ==":white_large_square:":
+                    on_message.emarr[1][2] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 8:
+                if on_message.emarr[2][0] ==":white_large_square:":
+                    on_message.emarr[2][0] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            elif i == 9:
+                if on_message.emarr[2][1] ==":white_large_square:":
+                    on_message.emarr[2][1] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+            else:
+                if on_message.emarr[2][2] ==":white_large_square:":
+                    on_message.emarr[2][2] = v
+                    new = discord.Embed()
+                    new.add_field(
+                        name=("{} vs. {}".format(str(userID[0]), str(userID[1]))),
+                        value=on_message.emarr[0][0]
+                        + " | "
+                        + on_message.emarr[0][1]
+                        + " | "
+                        + on_message.emarr[0][2]
+                        + "\n \n"
+                        + on_message.emarr[1][0]
+                        + " | "
+                        + on_message.emarr[1][1]
+                        + " | "
+                        + on_message.emarr[1][2]
+                        + "\n \n"
+                        + on_message.emarr[2][0]
+                        + " | "
+                        + on_message.emarr[2][1]
+                        + " | "
+                        + on_message.emarr[2][2],
+                    )
+                    await on_message.em.edit(embed=new)
+on_reaction_add.j = -1
 
 
 client.run(token)
